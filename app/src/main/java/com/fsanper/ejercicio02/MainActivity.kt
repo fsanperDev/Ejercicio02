c7d653b7cc67436aade16ebb537f2ebb48a62855
AlumDAM2M <fsanper617@g.educaand.es>
2023-09-28 11:07:30 +0200

    Box, Column, Row

--- app/src/main/java/com/fsanper/ejercicio02/MainActivity.kt
+++ app/src/main/java/com/fsanper/ejercicio02/MainActivity.kt
@@ -3,16 +3,28 @@ package com.fsanper.ejercicio02
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.fsanper.ejercicio02.ui.theme.Ejercicio02Theme
 
 class MainActivity : ComponentActivity() {
@@ -25,7 +37,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MyBox()
+                    MyRow()
                 }
             }
         }
@@ -43,18 +55,73 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(
     showBackground = true,
     showSystemUi = true,
-    device = Devices.NEXUS_5
+    device = Devices.NEXUS_5,
+    fontScale = 1f
 )
 @Composable
 fun GreetingPreview() {
     Ejercicio02Theme {
-        MyBox()
+        MyRow()
+    }
+}
+
+fun ColorAleatorio() {
+    Modifier.background(Color(4))
+}
+
+@Composable
+fun MyColumn() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.Center
+    ) {
+        repeat(4) {
+            Text(
+                text = "Ejemplo 1",
+                modifier = Modifier
+                    .background(Color.Red)
+                    .fillMaxWidth()
+                    .height(50.dp)
+            )
+        }
     }
 }
 
 @Composable
-fun MyBox(){
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
-        Text(text = "Esto es un texto de ejemplo")
+fun MyRow() {
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .horizontalScroll(rememberScrollState())
+    ) {
+        repeat(14) {
+            Text(
+                text = "Ejemplo 1",
+                modifier = Modifier
+                    .width(100.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun MyBox() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.DarkGray),
+        contentAlignment = Alignment.Center
+    ) {
+        Box(
+            modifier = Modifier
+                .width(250.dp)
+                .height(250.dp)
+                .background(Color.Cyan), contentAlignment = Alignment.Center
+        ) {
+            Text(text = "Esto es un ejemplo")
+        }
+
     }
 }
